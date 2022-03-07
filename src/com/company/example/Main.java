5cbafaf8574510db8962829864aa73478e89d405
012131 <muhammedfatih.aksu@turktelekom.com.tr>
2022-03-07 20:32:06 +0300

    first commit

---
+++ src/com/company/example/Main.java
@@ -0,0 +1,12 @@
+package com.company.example;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    System.out.println("first version");
+
+        Print printClass = new Print();
+        printClass.printSomething("second version");
+    }
+
+}
---
+++ src/com/company/example/Print.java
@@ -0,0 +1,7 @@
+package com.company.example;
+
+public class Print {
+    public void printSomething(String text){
+        System.out.println((text));
+    }
+}
